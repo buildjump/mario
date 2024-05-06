9cd9e471404fc40c387ee88d5b8badd8e8edc8d1
buildjump <169048140+buildjump@users.noreply.github.com>
2024-05-06 13:10:25 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ images.g.ts
@@ -0,0 +1,63 @@
+// Code généré automatiquement. Ne pas modifier.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "luigi":return img`
+. . . 7 7 7 7 7 7 7 . . . . . . 
+. . . 7 d d d d d 7 7 7 . . . . 
+. . . . d 9 d 9 d . . . . . . . 
+. . . . d d d d d . . . . . . . 
+. . . . d d d d d . . . . . . . 
+. . . . . 7 d 7 . . . . . . . . 
+. . . . f 7 7 7 f . . . . . . . 
+. . . . 7 f 7 f 7 . . . . . . . 
+. . . . 7 f 7 f 7 . . . . . . . 
+. . . . 7 f 7 f 7 . . . . . . . 
+. . . . 7 f f f 7 . . . . . . . 
+. . . . f f 5 f f . . . . . . . 
+. . . . 7 7 7 7 7 7 . . . . . . 
+. . . . 7 7 . . 7 7 . . . . . . 
+. . . . f f . . f f . . . . . . 
+. . . . f f f . f f f . . . . . 
+`;
+            case "image2":
+            case "mario":return img`
+. . . 2 2 2 2 2 2 2 . . . . . . 
+. . . 2 d d d d d 2 2 2 . . . . 
+. . . . d 9 d 9 d . . . . . . . 
+. . . . d d d d d . . . . . . . 
+. . . . d d d d d . . . . . . . 
+. . . . . 2 d 2 . . . . . . . . 
+. . . . 8 2 2 2 8 . . . . . . . 
+. . . . 2 8 2 8 2 . . . . . . . 
+. . . . 2 8 2 8 2 . . . . . . . 
+. . . . 2 8 2 8 2 . . . . . . . 
+. . . . 2 8 8 8 2 . . . . . . . 
+. . . . 8 8 5 8 8 . . . . . . . 
+. . . . 2 2 2 2 2 2 . . . . . . 
+. . . . 2 2 . . 2 2 . . . . . . 
+. . . . f f . . f f . . . . . . 
+. . . . f f f . f f f . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Code généré automatiquement. Ne pas modifier.
---
+++ main.ts
@@ -0,0 +1,2 @@
+let mario = sprites.create(assets.image`mario`, SpriteKind.Player)
+let luigi = sprites.create(assets.image`luigi`, SpriteKind.Player)
